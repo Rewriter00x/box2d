ebaa97693e7101201ab1c2516683bb582cafc28a
Rewriter00x <84532061+Rewriter00x@users.noreply.github.com>
2025-02-12 10:01:34 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "Box2D"
     kind "StaticLib"
-    language "C++"
-    cppdialect "C++11"
+    language "C"
+    cppdialect "C11"
     staticruntime "off"
 
     targetdir   ("bin/" .. outputdir .. "/%{prj.name}")
