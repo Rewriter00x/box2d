bd2811cdc61ad82ef0e9d9d23a7a87c3a41c6e22
Rewriter00x <84532061+Rewriter00x@users.noreply.github.com>
2025-02-12 09:32:09 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++11"
+    staticruntime "off"
+
+    targetdir   ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir      ("int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp",
+        "include/**.h",
+    }
+
+    includedirs
+    {
+        "include",
+        "src",
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter { "system:windows", "configurations:Debug" }
+        runtime "Debug"
+        symbols "on"
+
+    filter { "system:windows", "configurations:Release" }
+        runtime "Release"
+        optimize "on"
+
+    filter { "system:windows", "configurations:Dist" }
+        runtime "Release"
+        optimize "on"
